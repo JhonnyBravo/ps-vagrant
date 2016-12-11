64d1962f5e18d2aeb9c0cdfed76756c19e6574b4
JhonnyBravo <sanfranceshika5@gmail.com>
2016-12-11 15:42:34 +0900

    modified: get_package.ps1
    ヘルプテキストを追加。

--- get_package.ps1
+++ get_package.ps1
@@ -6,3 +6,15 @@ $url=$(Get-Content $path)
 $dst=$(Split-Path $url -Leaf)
 
 wget $url -OutFile $dst
+
+
+<#
+.SYNOPSIS
+URL リストに記載された URL から現在のディレクトリへパッケージをダウンロードします。
+
+.DESCRIPTION
+URL リストに記載された URL から現在のディレクトリへパッケージをダウンロードします。
+
+.PARAMETER path
+URL リストのパス。
+#>
